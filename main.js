69143936f533ead33d67c2d5be374c4204b6dfcf
MilenaGarcia <you@example.com>
2024-05-20 19:45:41 -0300

    renderizacion

--- main.js
+++ main.js
@@ -5,25 +5,49 @@ const errorMensage = document.getElementById("mensagge")
 const cardContainer = document.getElementById("container")
 
 
+const createCard = (pokemon) => {
+    const { name, types, weight, height } = pokemon;
+
+    const typesString = types.map(typeInfo => typeInfo.type.name).join('-');
+    
+    return cardContainer.innerHTML = `
+        <div id="card-info">
+            <h2>${name}</h2>
+            <p>Tipo: ${typesString}</p>
+            <p>Peso: ${weight / 10} kg</p>
+            <p>Altura: ${height / 10} m</p>
+            <img src="${pokemon.sprites.front_default}" alt="${name}">
+        </div>
+    `;
+   
+}
+
+
 const searchPokemon = async (e) =>{
     e.preventDefault();
 
     const inputValue = inputSearch.value.trim();
+
+    const pokemonResult = await requestPokemon(inputValue)
 
     if (inputValue == "") {
     errorMensage.innerText = `Por favor ingrese un numero`
     return
     }
     
-    const pokemonFetch = await (inputValue);
+    // const pokemonFetch = await (inputValue);
 
-    if (!pokemonFetch.id) {
+    if (!pokemonResult) {
         errorMensage.innerText =`no es posible encontrar a ese pokemon`
         return 
-    }    
+    }else{
+        errorMensage.innerText =``
+
+        return createCard(pokemonResult)
+    }   
+    
+    
 }
-
-
 
 Init = () => {
     formSearch.addEventListener('submit',searchPokemon)
