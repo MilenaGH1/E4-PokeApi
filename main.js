d48fea8c3a5f24bbc0cb609cb1a5da865a3ef161
MilenaGarcia <you@example.com>
2024-05-19 23:13:31 -0300

    logica de validacion

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-const inputSearch = document.getElementById("information")
-const btnSearch = document.getElementById("button")
+const formSearch = document.getElementById("search")
+const inputSearch = document.querySelector("#information")
+const btnSearch = document.querySelector("#button")
+const errorMensage = document.getElementById("mensagge")
+const cardContainer = document.getElementById("container")
+
+
+const searchPokemon = async (e) =>{
+    e.preventDefault();
+
+    const inputValue = inputSearch.value.trim();
+
+    if (inputValue == "") {
+    errorMensage.innerText = `Por favor ingrese un numero`
+    return
+    }
+    
+    const pokemonFetch = await (inputValue);
+
+    if (!pokemonFetch.id) {
+        errorMensage.innerText =`no es posible encontrar a ese pokemon`
+        return 
+    }    
+}
 
 
 
 Init = () => {
-    
+    formSearch.addEventListener('submit',searchPokemon)
 }
+
+Init()
--- request.js
+++ request.js
@@ -5,11 +5,13 @@ const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 const requestPokemon = async (number) => {
     try {
       const response = await fetch(
-        `${BASE_URL}?q=${number}&units=metric&lang=es`
+        `${BASE_URL}${number}`
       );
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error(`Hubo un error en la petition: ${error}`);
+      console.error(`Hubo un error en la peticion: ${error}`);
     }
-  };
+};
+
+
